aa6098187aa7e8010860b3814132bbc605c0e387
Mateusz Chmielewski <mateusz.chmielewski1@protonmail.com>
2022-04-17 22:21:32 +0200

    Add weather repository and get current weather weather for city function

---
+++ app/src/main/java/com/mateuszchmielewski/forecastapp/api/WeatherRepository.kt
@@ -0,0 +1,28 @@
+package com.mateuszchmielewski.forecastapp.api
+
+import android.content.res.Resources
+import com.mateuszchmielewski.forecastapp.R
+import com.mateuszchmielewski.forecastapp.model.ResponseType
+import retrofit2.HttpException
+import java.io.IOException
+
+class WeatherRepository {
+    private val api = ApiInstance.api
+    private val resources = Resources.getSystem()
+
+    suspend fun getCurrentWeatherForCity(city: String): ResponseType {
+        val response = try {
+            api.getCurrentWeatherForCity(city)
+        } catch (exception: IOException) {
+            return ResponseType.Error(resources.getString(R.string.io_exception))
+        } catch (exception: HttpException) {
+            return ResponseType.Error(resources.getString(R.string.http_exception))
+        }
+
+        if (response.isSuccessful && response.body() != null) {
+            return ResponseType.Success(response.body()!!)
+        }
+
+        return  ResponseType.Error(resources.getString(R.string.null_data_exception))
+    }
+}
